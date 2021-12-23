9732b23ac392e70f981b5f7cec3707326384148e
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 17:52:11 -0300

    resolve o execicio 006

--- Modulo-01/Tratando dados e fazendo contas/Ex006.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex006.py
@@ -1,3 +1,11 @@
 """Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada."""
 
+n = int(input('Digite um número: '))
 
+dobro = n * 2
+triplo = n * 3
+raiz = n / 0.5
+
+print(f'O dobro de {n} é {n * 2}')
+print(f'O triplo do {n} é {n * 3}')
+print(f'A raiz quedrada de {n} é {n ** 0.5}')
