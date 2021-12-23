309a4462a2046bef7750ecd9668bc3e31b262d51
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 17:13:55 -0300

    resolve o execicio 003

--- Modulo-01/Tratando dados e fazendo contas/Ex003.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex003.py
@@ -1,1 +1,6 @@
 """Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles."""
+
+numero1 = input('Digite um valor: ')
+numero2 = input('Digite outro valor: ')
+
+print(f'A some ente {numero1} e {numero2} é {numero1 + numero2}')
