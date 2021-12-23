a4badab239e6b18856e4ff875015d9a0aff69bfe
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 16:58:45 -0300

    termina o exercicio 002

--- Modulo-01/Primeiros passos com python/Ex002.py
+++ Modulo-01/Primeiros passos com python/Ex002.py
@@ -1,1 +1,4 @@
 """Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas. """
+
+nome = input('Digite seu nome: ')
+print(f'É um prazer em te conhecer {nome}!')
