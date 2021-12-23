a48bd8a14e619a7050ddb32fc3663fc09692afaa
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 17:18:31 -0300

    Cria execicio 004

--- Modulo-01/Tratando dados e fazendo contas/Ex003.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex003.py
@@ -1,6 +1,6 @@
 """Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles."""
 
-numero1 = input('Digite um valor: ')
-numero2 = input('Digite outro valor: ')
+numero1 = int(input('Digite um valor: '))
+numero2 = int(input('Digite outro valor: '))
 
 print(f'A some ente {numero1} e {numero2} é {numero1 + numero2}')
