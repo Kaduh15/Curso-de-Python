7a0194f4366b0aa8ffd177b334f3e1389e1b86df
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 17:34:18 -0300

    resolve o execicio 004

--- Modulo-01/Tratando dados e fazendo contas/Ex004.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex004.py
@@ -1,1 +1,11 @@
 """Exercício Python 4: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele."""
+
+n = input('Digite algo: ')
+
+print(f'O tipo primitivo desse valor é {type(n)}')
+print(f'Só tem espaços? {n.isspace()}')
+print(f'É um número? {n.isnumeric()}')
+print(f'É alfabético? {n.isalpha()}')
+print(f'Está em maiúsculas? {n.isupper()}')
+print(f'Está em minúsculas? {n.islower()}')
+print(f'está caítalizada? {n.istitle()}')
