229489c2f075d566540c0ad9461df30a1948e983
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 17:42:52 -0300

    resolve o execicio 005

--- Modulo-01/Tratando dados e fazendo contas/Ex005.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex005.py
@@ -1,3 +1,5 @@
 """Exercício Python 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor."""
 
+n = int(input('Digite um número: '))
 
+print(f'Analisando o valor {n}, seu antecessor é {n-1} e seu sucessor é {n+1}')
