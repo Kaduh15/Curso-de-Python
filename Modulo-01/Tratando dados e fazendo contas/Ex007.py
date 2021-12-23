75476f57fe9aef3dd8d039ac18acb6a31ba3d052
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-23 18:02:19 -0300

    Resolve o exercicio 007

---
+++ Modulo-01/Tratando dados e fazendo contas/Ex007.py
@@ -0,0 +1,6 @@
+"""Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média."""
+
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: '))
+
+print(f'A média entre {nota1} e {nota2} é igual a {(nota1 + nota2) / 2}')
