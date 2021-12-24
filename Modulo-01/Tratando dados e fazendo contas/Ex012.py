167197f67f799daf3e7f7350710ec41b5e7d2c17
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-24 11:51:12 -0300

    resolve o exercicio 012

--- Modulo-01/Tratando dados e fazendo contas/Ex012.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex012.py
@@ -1,3 +1,7 @@
 """Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto."""
 
+produto = float(input('Qual o preço do produto? R$'))
 
+desconto = produto * 0.05
+
+print(f'O produto que custava R${produto}, na promoção com desconto de 5% vai ficar R${(produto - desconto):.2f}')
