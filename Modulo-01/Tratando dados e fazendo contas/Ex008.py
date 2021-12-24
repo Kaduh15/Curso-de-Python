b6448bc9be44a4887300acb10901ea1f92110b8b
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-24 11:15:49 -0300

    resolve o exercicio 008

--- Modulo-01/Tratando dados e fazendo contas/Ex008.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex008.py
@@ -1,3 +1,11 @@
 """Exercício Python 8: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros."""
 
+distancia = float(input('Digite uma distância em metros: '))
 
+print(f'A medida de {distancia}m corresponde a')
+print(f'{distancia / 1000}km')
+print(f'{distancia / 100}hm')
+print(f'{distancia / 10}dam')
+print(f'{distancia * 10}dm')
+print(f'{distancia * 100}cm')
+print(f'{distancia * 1000}mm')
