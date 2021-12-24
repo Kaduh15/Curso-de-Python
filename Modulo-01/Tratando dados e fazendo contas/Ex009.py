94b9540ad4f7a6af745f8ca04c1f14fd1fa89c6c
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-24 11:27:51 -0300

    resolve o exercicio 009

--- Modulo-01/Tratando dados e fazendo contas/Ex009.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex009.py
@@ -1,3 +1,14 @@
 """Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada."""
 
+numero = int(input('Digite um número para ver sua tabuada: '))
 
+print(f'{numero} X {1:2} = {numero * 1}')
+print(f'{numero} X {2:2} = {numero * 2}')
+print(f'{numero} X {3:2} = {numero * 3}')
+print(f'{numero} X {4:2} = {numero * 4}')
+print(f'{numero} X {5:2} = {numero * 5}')
+print(f'{numero} X {6:2} = {numero * 6}')
+print(f'{numero} X {7:2} = {numero * 7}')
+print(f'{numero} X {8:2} = {numero * 8}')
+print(f'{numero} X {9:2} = {numero * 9}')
+print(f'{numero} X {10:2} = {numero * 10}')
