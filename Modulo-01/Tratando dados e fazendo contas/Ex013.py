0cd9a63c3c8799e7349fa27d42d5297212546b43
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-24 11:56:15 -0300

    resolve o execicio 013

--- Modulo-01/Tratando dados e fazendo contas/Ex013.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex013.py
@@ -1,3 +1,7 @@
 """Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento."""
 
+salario_atual = float(input('Digite o salario atual: R$'))
 
+novo_salario = (salario_atual * 0.15) + salario_atual
+
+print(f'Um funcionário que ganhava R${salario_atual}, com 15% de aumento, passa a receber R${novo_salario:.2f}')
