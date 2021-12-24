27446ee326c28406723e45110ed693b264838e03
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-24 11:34:51 -0300

    resolve o exercicio 010

--- Modulo-01/Tratando dados e fazendo contas/Ex010.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex010.py
@@ -1,3 +1,5 @@
 """Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. """
 
+dinheiro = float(input('Quanto dinheiro você tem na certeira? R$'))
 
+print(f'Com R${dinheiro} você pode comprar US${dinheiro / 5.68:.2f}')
