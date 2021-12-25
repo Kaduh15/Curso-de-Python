56c4ed4f0dcf5f99735c0e3c45b39f187f42ee5b
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-25 11:24:24 -0300

    resolve o exercicio 014

--- Modulo-01/Tratando dados e fazendo contas/Ex014.py
+++ Modulo-01/Tratando dados e fazendo contas/Ex014.py
@@ -1,3 +1,7 @@
 """Exercício Python 14: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit."""
 
+grua_celsius = float(input('Qual a temperatura em °C: '))
 
+fahrenheit = (grua_celsius * 1.8) + 32
+
+print(f'A temperatura de {grua_celsius}°C corresponde a {fahrenheit}°F!')
