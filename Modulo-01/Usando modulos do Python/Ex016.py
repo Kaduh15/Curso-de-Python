efe011c03529c03f9e430243330df0e726915cdd
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-26 09:05:02 -0300

    resolve o exercicio 016

--- Modulo-01/Usando modulos do Python/Ex016.py
+++ Modulo-01/Usando modulos do Python/Ex016.py
@@ -1,3 +1,5 @@
 """Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira."""
 
+num = float(input('Digite um valor: '))
 
+print(f'O valor digitado foi {num} e sua porção inteira é {int(num)}')
