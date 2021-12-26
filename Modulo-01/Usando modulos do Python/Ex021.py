7fac0f07f22e7a0f78c0b11f364040d135a747ab
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-26 10:26:38 -0300

    Instala o pygame

--- Modulo-01/Usando modulos do Python/Ex021.py
+++ Modulo-01/Usando modulos do Python/Ex021.py
@@ -1,3 +1,8 @@
 """Exercício Python 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3."""
 
+import pygame
 
+pygame.init()
+pygame.mixer.music.load('/home/kadu/Documentos/GitHub/Curso-de-Python/Modulo-01/Usando modulos do Python/musga.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
