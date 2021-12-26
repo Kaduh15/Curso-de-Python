3cb732b183b04353594001047879b0b080662385
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-26 09:34:11 -0300

    cria o execicio 019

---
+++ Modulo-01/Usando modulos do Python/Ex019.py
@@ -0,0 +1,3 @@
+"""Exercício Python 19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido."""
+
+
