12ef45a1edf2c446ddf664c45108d452a61a2d12
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-26 09:14:20 -0300

    resolve o exercicio 017

--- Modulo-01/Usando modulos do Python/Ex017.py
+++ Modulo-01/Usando modulos do Python/Ex017.py
@@ -1,3 +1,8 @@
 """Exercício Python 17: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa."""
 
+from math import hypot
 
+co = float(input('Digite o valor do Cateto Oposto: '))
+ca = float(input('Digite o valor do Cateto Adjacente: '))
+
+print(f'O valor da hipotenusa é {hypot(co, ca):.2f}')
