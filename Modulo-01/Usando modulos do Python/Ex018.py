f39464ef0ad701d057421e38909d09a26e1a18cb
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-26 09:31:03 -0300

    resolve o exercicio 018

--- Modulo-01/Usando modulos do Python/Ex018.py
+++ Modulo-01/Usando modulos do Python/Ex018.py
@@ -1,3 +1,14 @@
 """Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo. """
 
+from math import sin, cos, tan, radians
 
+angulo = int(input('Digite o valor do Ângulo: '))
+
+seno = sin(radians(angulo))
+coss = cos(radians(angulo))
+tang = tan(radians(angulo))
+
+print(f'O ângulo de {angulo}° tem o')
+print(f'Seno de {seno:.2f}° Graus')
+print(f'Cosseno de {coss:.2f}° Gruas')
+print(f'Tangente de {tang:.2f}° Graus')
