5f041b250541c3b2b2770aa1300b9b26dc09db67
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-27 21:31:14 -0300

    resolve o exercicio 025

--- Modulo-01/Manipulando Texto/Ex025.py
+++ Modulo-01/Manipulando Texto/Ex025.py
@@ -1,3 +1,5 @@
 """Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome."""
 
+nome = input('Digite seu nome completo: ').strip().lower()
 
+print(f'Seu nome tem "Silva"? {"silva" in nome }')
