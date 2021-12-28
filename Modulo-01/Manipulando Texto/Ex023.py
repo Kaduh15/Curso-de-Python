e6cb2b6af28c52f06c0642919c263d94262ac6c8
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-27 21:19:59 -0300

    resolve o exercicio 023

--- Modulo-01/Manipulando Texto/Ex023.py
+++ Modulo-01/Manipulando Texto/Ex023.py
@@ -1,3 +1,14 @@
 """Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados."""
 
+num = int(input('Digite um número: '))
 
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+
+print(f'Analizando o número {num}')
+print(f'Unidade: {unidade}')
+print(f'Dezena: {dezena}')
+print(f'Centena: {centena}')
+print(f'Milhar: {milhar}')
