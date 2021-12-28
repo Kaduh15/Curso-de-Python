eb27377402ea5895c8fc9cf2d263865832de1c64
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-27 21:24:15 -0300

    resolve o exercicio 024

--- Modulo-01/Manipulando Texto/Ex024.py
+++ Modulo-01/Manipulando Texto/Ex024.py
@@ -1,3 +1,5 @@
 """Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”."""
 
+cidade = input('Didige o nome da cidade: ').strip().upper()
 
+print('SANTO' in cidade.split()[0])
