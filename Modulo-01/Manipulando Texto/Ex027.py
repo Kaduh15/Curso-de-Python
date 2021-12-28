5535fa775ad701a8cd9f40c628d3684867c0b946
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-27 21:54:59 -0300

    resolve o exercicio 027

--- Modulo-01/Manipulando Texto/Ex027.py
+++ Modulo-01/Manipulando Texto/Ex027.py
@@ -1,3 +1,5 @@
 """Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente. """
 
+nome = input('Digite seu nome completo: ').strip().title()
 
+print(f'Seu primeiro nome é {nome.split()[0]} e seu último nome é {nome.split()[-1]}')
