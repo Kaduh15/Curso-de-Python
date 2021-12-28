5721a62b4abd0ab4060a2bdfae3807018c416137
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-27 21:04:30 -0300

    resolve o exercicio 022

--- Modulo-01/Manipulando Texto/Ex022.py
+++ Modulo-01/Manipulando Texto/Ex022.py
@@ -3,4 +3,11 @@
 – Quantas letras ao todo (sem considerar espaços).
 – Quantas letras tem o primeiro nome."""
 
+nome = input('Digite seu nome completo: ').strip()
 
+print(f'''
+Seu nome em Maiúsculas: {nome.upper()}
+Seu nome em Minúsculas: {nome.lower()}
+Seu nome tem {len(''.join(nome))} letras
+seu primeiro nome tem {len(nome.split()[0])} letras
+''')
