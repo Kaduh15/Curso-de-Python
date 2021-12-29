d0522b140815dc0dda5679ee600881513403048e
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-29 20:27:10 -0300

    resolve o exercicio 028

--- Modulo-01/Condição/Ex028.py
+++ Modulo-01/Condição/Ex028.py
@@ -1,3 +1,14 @@
 """Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu."""
 
+from random import randint
 
+pc = randint(0, 5)
+
+num = int(input("""Olá, eu pensei em número entre 0 e 5. consegue adivinhar?
+Qual número eu pensei? 2
+"""))
+
+if num == pc:
+    print('Você acertou!!!')
+else:
+    print('Você perdeu!!!')
