230a9e67edacb27cf13acb5b71db107a55f55293
Kaduh15 <kadu.silva2014@gmail.com>
2021-12-29 20:41:46 -0300

    resolve o exercicio 030

--- Modulo-01/Condição/Ex030.py
+++ Modulo-01/Condição/Ex030.py
@@ -1,3 +1,8 @@
 """Exercício Python 30: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR."""
 
+num = int(input('Digite um valor: '))
 
+if num % 2 == 0:
+    print(f'O número {num} é PAR!!')
+else:
+    print(f'O número {num} é IMPAR!!!')
