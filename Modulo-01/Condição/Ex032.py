1cb46ad630031c0040c71c3fb18c9397da902108
Kaduh15 <kadu.silva2014@gmail.com>
2022-01-04 20:42:04 -0300

    resolve o exercicio 032

--- Modulo-01/Condição/Ex032.py
+++ Modulo-01/Condição/Ex032.py
@@ -1,3 +1,17 @@
 """Exercício Python 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto."""
 
+from datetime import date
 
+ano_atual = date.today().year
+
+print(ano_atual)
+
+ano = int(input('Digite o ano para saber se ele é bissexto: '))
+
+if ano == 0:
+    ano = ano_atual
+
+if ano % 4 == 0 and int(ano / 100) % 4 == 0:
+    print(f'{ano} é ano bissexto!')
+else:
+    print(f'{ano} não é ano bissexto!')
