812353cd66c775a90f220534e7388ebc72e4de4c
Kaduh15 <kadu.silva2014@gmail.com>
2022-01-04 17:23:25 -0300

    resolve o exercicio 031

--- Modulo-01/Condição/Ex031.py
+++ Modulo-01/Condição/Ex031.py
@@ -1,3 +1,10 @@
 """Exercício Python 31: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas."""
 
+distancia = float(input('Qual a distância da sua viagem? '))
 
+if distancia <= 200:
+    valor_viagem = distancia * 0.50
+else:
+    valor_viagem = distancia * 0.45
+    
+print(f'O valor total da viagem é de R${valor_viagem} reais!')
