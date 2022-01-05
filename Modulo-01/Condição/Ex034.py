4ba4a917d4cb8cb198688691c5b8486a74850375
Kaduh15 <kadu.silva2014@gmail.com>
2022-01-04 21:12:44 -0300

    resolve o exercicio 034

--- Modulo-01/Condição/Ex034.py
+++ Modulo-01/Condição/Ex034.py
@@ -1,3 +1,10 @@
 """"Exercício Python 34: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%."""
 
+salario = float(input('Digite o valor do salário: R$'))
 
+if salario > 1250:
+    novo_salario = (salario * 0.10) + salario
+else:
+    novo_salario = (salario * 0.15) + salario
+
+print(f'O novo salario é de R${novo_salario} reais')
