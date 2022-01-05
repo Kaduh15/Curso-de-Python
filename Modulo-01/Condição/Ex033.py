5628fcd7042586fc429556d1800c172c4488abb8
Kaduh15 <kadu.silva2014@gmail.com>
2022-01-04 21:02:22 -0300

    resolve o exercicio 033

--- Modulo-01/Condição/Ex033.py
+++ Modulo-01/Condição/Ex033.py
@@ -1,3 +1,20 @@
 """Exercício Python 33: Faça um programa que leia três números e mostre qual é o maior e qual é o menor."""
 
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite putro número: '))
+num3 = int(input('Digite mais um número: '))
 
+maior = num1
+menor = num1
+
+if num2 > maior:
+    maior = num2
+elif num3 > maior:
+    maior = num3
+
+if num2 < menor:
+    menor = num2
+elif num3 < menor:
+    menor = num3
+
+print(f'O maior número foi {maior} e o menor foi {menor}')
