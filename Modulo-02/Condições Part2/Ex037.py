d9c71b60bec8af79b87d79c602a3eb105e10444d
Kaduh15 <kadu.silva2014@gmail.com>
2022-01-05 19:33:59 -0300

    cria o exercicio 037

---
+++ Modulo-02/Condições Part2/Ex037.py
@@ -0,0 +1,3 @@
+"""Exercício Python 37: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal. """
+
+
