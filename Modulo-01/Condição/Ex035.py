ea6876c83bd77212f8b38465801e3950aeca9fc5
Kaduh15 <kadu.silva2014@gmail.com>
2022-01-04 21:19:52 -0300

    resolve o exercicio 035

--- Modulo-01/Condição/Ex035.py
+++ Modulo-01/Condição/Ex035.py
@@ -1,3 +1,10 @@
 """Exercício Python 35: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo."""
 
+a = int(input('Digite o primeiro lado: '))
+b = int(input('Digite o segundo lado: '))
+c = int(input('Digite o Último lado: '))
 
+if ((b - c) < a < b + c) and ((a - c) < b < a + c) and ((a - b) < c < a + b):
+    print('É possivel fazer um triango com as médidas informadas!!')
+else:
+    print('Com as médidas informadas não é possivel forma um triangulo!!')
